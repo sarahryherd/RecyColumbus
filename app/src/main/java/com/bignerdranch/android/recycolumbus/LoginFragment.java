c6ff809b664e3c201de9b55cb2ddc4aaa0c79a01
John Morales <morales.201@osu.edu>
2018-10-01 11:48:39 -0400

    Beginning attempt at implementing toggle between Signup and Login

--- app/src/main/java/com/bignerdranch/android/recycolumbus/LoginFragment.java
+++ app/src/main/java/com/bignerdranch/android/recycolumbus/LoginFragment.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.recycolumbus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,12 +9,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class LoginFragment extends Fragment {
+
+    private ToggleButton mLoginToggle;
+    private ToggleButton mSignupToggle;
+    private Button mLoginButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mLoginToggle = v.findViewById(R.id.login_login_toggle);
+        mLoginToggle.setEnabled(false);
+
+        mSignupToggle = v.findViewById(R.id.login_signup_toggle);
+        mSignupToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupIntent = SignupActivity.newIntent(getActivity());
+                startActivity(signupIntent);
+            }
+        });
+
+        mLoginButton = v.findViewById(R.id.login_login_button);
+        mLoginButton.setEnabled(false);
+
+        return v;
     }
 }
