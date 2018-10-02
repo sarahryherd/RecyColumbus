5cd3174b92794fb2db16fa283590ec391121d12b
John Morales <morales.201@osu.edu>
2018-10-02 09:42:45 -0400

    Added lifecycle methods to SignupActivity

--- app/src/main/java/com/bignerdranch/android/recycolumbus/SignupActivity.java
+++ app/src/main/java/com/bignerdranch/android/recycolumbus/SignupActivity.java
@@ -7,9 +7,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class SignupActivity extends SingleFragmentActivity {
+    private static final String TAG = "SignupActivity";
 
     public static Intent newIntent(Context packageContext) {
         return new Intent(packageContext, SignupActivity.class);
@@ -18,6 +20,42 @@ public class SignupActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new SignupFragment();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
 }
