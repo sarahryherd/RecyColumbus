e218ac413ecd11bff864f94bc9a16c771d7093ff
sarahryherd <ryherd.1@osu.edu>
2018-10-22 11:38:36 -0400

    tried to fix gradle file and added google maps to gradle dependencies

--- app/src/main/java/com/bignerdranch/android/recycolumbus/MapActivity.java
+++ app/src/main/java/com/bignerdranch/android/recycolumbus/MapActivity.java
@@ -3,6 +3,8 @@ package com.bignerdranch.android.recycolumbus;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -39,7 +41,6 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
