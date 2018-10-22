9fbd0a8b49a54d4d101c3a1adb90d5242cb5dbd1
sarahryherd <ryherd.1@osu.edu>
2018-10-22 14:57:51 -0400

    added nav bar to map activity

--- app/src/main/java/com/bignerdranch/android/recycolumbus/MapActivity.java
+++ app/src/main/java/com/bignerdranch/android/recycolumbus/MapActivity.java
@@ -1,7 +1,11 @@
 package com.bignerdranch.android.recycolumbus;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.bignerdranch.android.recycolumbus.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,6 +18,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_information:
+                    mTextMessage.setText("InformationActivity");
+                    return true;
+                case R.id.navigation_location:
+                    mTextMessage.setText("Location");
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText("Search");
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText("Settings");
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
