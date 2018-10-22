08d733c1f859b180aa0e7fe92bf79c7825cd4724
sarahryherd <ryherd.1@osu.edu>
2018-10-22 14:54:04 -0400

    generated and added google maps API key

--- app/src/main/java/com/bignerdranch/android/recycolumbus/MapActivity.java
+++ app/src/main/java/com/bignerdranch/android/recycolumbus/MapActivity.java
@@ -1,12 +1,9 @@
 package com.bignerdranch.android.recycolumbus;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.support.v4.app.FragmentActivity;
+
+import com.bignerdranch.android.recycolumbus.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,41 +13,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private TextView mTextMessage;
     private GoogleMap mMap;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_information:
-                    mTextMessage.setText("InformationActivity");
-                    return true;
-                case R.id.navigation_location:
-                    mTextMessage.setText("Location");
-                    return true;
-                case R.id.navigation_search:
-                    mTextMessage.setText("Search");
-                    return true;
-                case R.id.navigation_settings:
-                    mTextMessage.setText("Settings");
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_activity);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -63,5 +33,4 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
 }
