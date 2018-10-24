f0a0edd03ff9f389b2f4417442f2d89944c1f255
Yiwei Zhang <maggie.zyw@gmail.com>
2018-10-24 10:47:02 -0400

    Get user email from Firebase

--- app/src/main/java/com/bignerdranch/android/recycolumbus/SettingsActivity.java
+++ app/src/main/java/com/bignerdranch/android/recycolumbus/SettingsActivity.java
@@ -1,15 +1,27 @@
 package com.bignerdranch.android.recycolumbus;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+import static android.app.PendingIntent.getActivity;
 
 public class SettingsActivity extends SingleFragmentActivity {
 
     private Button delete_button;
+    private TextView emailText;
 
     @Override
     protected Fragment createFragment() {
@@ -21,27 +33,43 @@ public class SettingsActivity extends SingleFragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         delete_button = (Button) findViewById(R.id.deleteAcctButt);
+        emailText = (TextView) findViewById(R.id.emailText);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String email = user.getEmail();
 
-        AlertDialog.Builder alert = new AlertDialog.Builder(SettingsActivity.this);
-        alert.setTitle("Delete");
-        alert.setMessage("Are you sure you want to delete the account?");
-        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 
+
+        delete_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(SettingsActivity.this);
+                alert.setTitle("Delete");
+                alert.setMessage("Are you sure you want to delete the account?");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(SettingsActivity.this, HomePageActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog temp = alert.create();
+                temp.show();
+
             }
         });
-
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
         
 
     }
+
 }
