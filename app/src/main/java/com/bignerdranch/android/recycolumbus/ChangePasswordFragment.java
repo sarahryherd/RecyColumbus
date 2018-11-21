397e513a0d12d53075dfc605f2c979d849df4dbe
John Morales <morales.201@osu.edu>
2018-11-20 19:41:20 -0500

    Fixed merge issue

--- app/src/main/java/com/bignerdranch/android/recycolumbus/ChangePasswordFragment.java
+++ app/src/main/java/com/bignerdranch/android/recycolumbus/ChangePasswordFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.recycolumbus;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,6 +11,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
